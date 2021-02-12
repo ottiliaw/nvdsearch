0cabbed7702d3167caffbc65c0e062300299ae86
ottilia <ottiliawesterlund@ottilias-mbp.home>
2021-02-12 18:50:23 +0000

    Read local JSON + return correct data implented

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 Install script:
 1) Pull repo and cd into project folder.
 2) >npm install -g .
-3) >nvdsearch
+3) >nvdsearch --id <cve-id>
+
+General information:
+It uses CVSS V3 for the severity base score.
--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
+const yargs = require("yargs");
+const fs = require('fs');
 
-console.log( "Hello!" );
+const options = yargs
+ .usage("Usage: --id <cve-id>")
+ .option("id", { alias: "cveId", describe: "CVE ID", type: "string", demandOption: true })
+ .argv;
+
+const fetchData = () => {
+    const [ cve, year, identifier] = options.cveId.split('-');
+    let rawdata = fs.readFileSync(`nvdcve-1.1-${year}.json`);
+    let data = JSON.parse(rawdata);
+
+    const cveItem = data.CVE_Items;
+    
+    cveItem.forEach(cve => {
+        if ( cve.cve.CVE_data_meta.ID === options.cveId) {
+            console.log(options.cveId)
+            console.log(cve.cve.description.description_data[0].value)
+            console.log(cve.impact.baseMetricV3.cvssV3.baseScore + ' ' + cve.impact.baseMetricV3.cvssV3.vectorString )
+        }
+    });
+}
+
+fetchData();
